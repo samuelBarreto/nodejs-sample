e9265ad7660067c5f4afc3902e7de0d8c5a1153f
samuel.andrade <samukacfc1@gmail.com>
2024-06-07 17:18:17 -0300

    teste

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-    it("should return 'Hello World DevOpsoooo!! :)'", async () => {
+    it("should return 'Hello World DevOpsoooo!!'", async () => {
         const res = await request(app)
             .get("/")
             .expect('Content-Type', /text/)
             .expect(200);
         
-        expect(res.text).toBe('Hello World DevOpsoooo!! :)');
+        expect(res.text).toBe('Hello World DevOpsoooo!!');
     });
 });
