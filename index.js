78c9223d01f950f9cb7c940322e0ff075af8c510
samuel.andrade <samukacfc1@gmail.com>
2024-06-10 17:38:03 -0300

    v.1.1 - sonar

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   response.send('Hello World DevOpsoooo!!');
 });
 
-//teste
+
 if (require.main === module) {
   app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
